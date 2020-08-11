2fba5727b348d8e90ffb0784cb33ca1b5e383da6
changyan-destoplab <changyanhe@126.com>
2020-08-11 14:12:36 +0800

    plot wireframe in callback function

--- scripts/GUI_wireframe_realtimeplot.py
+++ scripts/GUI_wireframe_realtimeplot.py
@@ -12,6 +12,7 @@ from numpy.random import rand
 import rospy
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits import mplot3d
 import numpy as np
 from std_msgs.msg import Float64
 
@@ -33,9 +34,303 @@ import cv2 as cv
 from visulization.msg import IntList
 import math
 
+
+
 datareceived = []
 cv_image = []
 exitflag = 1
+countgloabal = 1
+
+
+# use matplotlib plot wireframe in callback, to synchrinize data
+
+def callback(data):
+
+    # global cv_image
+    # # Datamatrix = data.data
+    # bridge = CvBridge()
+    # # rospy.loginfo("I heard %0.6f", data.data)
+    # try:
+    #   # cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    #   cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough') # output cv:mat
+    # except CvBridgeError as e:
+    #   print(e)
+    # cv.namedWindow("Image window", cv.WINDOW_NORMAL)
+    # cv.imshow("Image window", cv_image)
+    # cv.waitKey(3)
+
+    datareceived = data.data
+    print("I received:", datareceived)
+
+    griddense = 0.1
+    # define a small range to show spike signal nicely with cos function
+    R = 0.5
+    x_cut = np.arange(-R,R,griddense)
+    y_cut = np.arange(-R,R,griddense)
+    X_cut, Y_cut = np.meshgrid(x_cut,y_cut)
+    r = np.sqrt(X_cut**2 + Y_cut**2)
+    cut_len = int(2*R/griddense)
+    Z_cut = np.zeros([cut_len, cut_len])
+    for i in range(Z_cut.shape[0]):
+      for j in range(Z_cut.shape[1]):
+        Z_cut[i,j] = np.cos(math.pi*r[i,j]) if r[i,j]<=R else 0
+
+
+    if datareceived:
+
+      axisrange = datareceived[0]+1
+
+      X = np.arange(1, axisrange, griddense)
+      Y = np.arange(1, axisrange, griddense)
+      X, Y = np.meshgrid(X, Y)
+      datalen = int((axisrange-1)/griddense)
+      Z = np.zeros([datalen, datalen])
+
+      index_x = int((datareceived[1])/griddense)
+      index_y = int((datareceived[2])/griddense)
+      # Z[index_x, index_y] = datareceived[3]/100
+      # datareceived[1] = datareceived[1]+1
+      # datareceived[2] = datareceived[2]+1
+      Z_cut_current = np.zeros([cut_len, cut_len])
+      detI = datareceived[3]/100
+      Z_cut_current = detI*Z_cut  #+detI
+      # Z_cut_current = Z_cut+detI
+      # print("index_y:", index_y)
+      # print("index_y-cut_len/2:", index_y-cut_len/2)
+
+      Z[int(index_x-cut_len/2):int(index_x+cut_len/2),int(index_y-cut_len/2):int(index_y+cut_len/2)] = Z_cut_current
+
+    
+
+    else:
+      X = np.arange(1, 5, 0.1)
+      Y = np.arange(1, 5, 0.1)
+      X, Y = np.meshgrid(X, Y)
+      R = np.sqrt(X ** 2 + Y ** 2)
+        # Z = np.sin(R+Datamatrix)
+      Z = np.sin(R)
+
+    wf = plt.axes(projection = '3d')
+    wf.plot_wireframe(X, Y, Z, rstride=1, cstride=1)
+
+
+    plt.annotate('local max', xyz=(index_x,index_y,Z_cut_current), xyztext=(index_x,index_y,(Z_cut_current+5)), 
+            xycoords="data", textcoords="axes fraction",
+            bbox={'facecolor':'w','pad':5}, ha="right", va="top")
+
+    plt.draw()
+    plt.pause(0.0000001)
+
+
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+        
+    rospy.init_node('GUI_listener', anonymous=True)
+    rospy.Subscriber("fiber_index", IntList, callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    plt.ion()
+    plt.show()
+    rospy.spin()
+
+
+
+if __name__ == '__main__':
+    listener()
+
+
+
+'''
+# write gui plot into callback, doesnot work, error: main thread in not in main loop
+
+def draw_figure(canvas_plot, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas_plot)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
+
+def callback(data,args):
+    # global cv_image
+    # # Datamatrix = data.data
+    # bridge = CvBridge()
+    # # rospy.loginfo("I heard %0.6f", data.data)
+    # try:
+    #   # cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    #   cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough') # output cv:mat
+    # except CvBridgeError as e:
+    #   print(e)
+    # cv.namedWindow("Image window", cv.WINDOW_NORMAL)
+    # cv.imshow("Image window", cv_image)
+    # cv.waitKey(3)
+
+    # global exitflag
+    # global datareceived
+    # global countgloabal
+
+    datareceived = data.data
+    print("I received:", datareceived)
+
+    ax = args[0]
+    fig_agg = args[1]
+    window = args[2]
+
+    #############################################update wireframe range here
+    # global datareceived
+
+    # global exitflag
+
+    count = 0
+
+    griddense = 0.1
+    # define a small range to show spike signal nicely with cos function
+    R = 0.5
+    x_cut = np.arange(-R,R,griddense)
+    y_cut = np.arange(-R,R,griddense)
+    X_cut, Y_cut = np.meshgrid(x_cut,y_cut)
+    r = np.sqrt(X_cut**2 + Y_cut**2)
+    cut_len = int(2*R/griddense)
+    Z_cut = np.zeros([cut_len, cut_len])
+    for i in range(Z_cut.shape[0]):
+      for j in range(Z_cut.shape[1]):
+        Z_cut[i,j] = np.cos(math.pi*r[i,j]) if r[i,j]<=R else 0
+
+
+    # print("I am in while loop")
+    if datareceived:
+
+      axisrange = datareceived[0]+1
+
+      X = np.arange(1, axisrange, griddense)
+      Y = np.arange(1, axisrange, griddense)
+      X, Y = np.meshgrid(X, Y)
+      datalen = int((axisrange-1)/griddense)
+      Z = np.zeros([datalen, datalen])
+
+      index_x = int((datareceived[1])/griddense)
+      index_y = int((datareceived[2])/griddense)
+      # Z[index_x, index_y] = datareceived[3]/100
+      # datareceived[1] = datareceived[1]+1
+      # datareceived[2] = datareceived[2]+1
+      Z_cut_current = np.zeros([cut_len, cut_len])
+      detI = datareceived[3]/100
+      Z_cut_current = detI*Z_cut  #+detI
+      # Z_cut_current = Z_cut+detI
+      # print("index_y:", index_y)
+      # print("index_y-cut_len/2:", index_y-cut_len/2)
+
+      Z[int(index_x-cut_len/2):int(index_x+cut_len/2),int(index_y-cut_len/2):int(index_y+cut_len/2)] = Z_cut_current
+
+      # window['-OUTPUT-'].update(str([datareceived[1]+1,datareceived[2]+1,datareceived[3],datareceived[4]])) ####################update displayed data here
+
+
+    else:
+      X = np.arange(1, 5, 0.1)
+      Y = np.arange(1, 5, 0.1)
+      X, Y = np.meshgrid(X, Y)
+      R = np.sqrt(X ** 2 + Y ** 2)
+        # Z = np.sin(R+Datamatrix)
+      Z = np.sin(R+count)
+
+      window['-OUTPUT-'].update('Loanding') ####################update displayed data here
+
+
+    # event, values = window.read(timeout=10)
+    # if event in ('Exit', None):
+    #     # break
+    #   exitflag = 0
+    #   exit(69)
+       
+    # if event in ('Connect', None):
+    #   sg.popup_ok('Successful Connection')  # Shows OK button
+
+      
+    ax.cla()
+    ax.grid(True)
+    # R = np.sqrt(X ** 2 + Y ** 2)
+    # # Z = np.sin(R+Datamatrix)
+    # Z = np.sin(R+count)
+    # surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet,
+    #                    linewidth=0, antialiased=False)
+    ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1)
+    # ax.set_zlim3d(-1.01, 1.01)
+
+# ax.w_zaxis.set_major_locator(LinearLocator(10))
+# ax.w_zaxis.set_major_formatter(FormatStrFormatter('%.03f'))
+
+    # fig.colorbar(surf, shrink=0.5, aspect=5) ## Show colorbar
+    ax.legend()
+    fig_agg.draw()
+    count = count + 1
+
+
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+
+    sg.theme('LightGreen')
+    figure_w = 650
+    figure_h = 650
+    # define the form layout
+    layout = [[sg.Text('Demo - Pressure Response',  justification='center', font='Helvetica 20')],
+              [sg.Canvas(size=(figure_w, figure_h), key='-CANVAS_plot-'), 
+              # sg.Canvas(size=(figure_w, figure_h), key='-CANVAS_image-') ,
+              ],
+              # [sg.Text('Force value and coordinates:',size=(40, 1),font='Helvetica 20'),
+              [sg.Text('Force value and coordinates:',font='Helvetica 20'),
+              sg.Text( 'loading:',font='Helvetica 20',size=(40, 1), key='-OUTPUT-')],
+              # [[sg.Button('Connect',size=(10, 2), pad=((280, 20), 3), font='Helvetica 14')],
+              [sg.Button('Connect',size=(10, 2), font='Helvetica 14'),
+              sg.Button('Exit', size=(10, 2),  font='Helvetica 14')]]
+
+    # create the form and show it without the plot
+    window = sg.Window('Demo - Pressure Response', layout, resizable=True, finalize=True)
+
+    # image_elem = window['-CANVAS_image-']
+
+    canvas_plot_elem = window['-CANVAS_plot-']
+    canvas_plot = canvas_plot_elem.TKCanvas
+    # draw the intitial scatter plot
+    fig = plt.figure(figsize=(8,6))
+
+    ax = fig.add_subplot(111, projection='3d')
+    # fig, ax = plt.subplots(111, projection='3d')
+    # ax.grid(True)
+    fig_agg = draw_figure(canvas_plot, fig)
+    
+
+
+    rospy.init_node('GUI_listener', anonymous=True)
+    # rospy.Subscriber("image_raw", image_msg, callback)
+    rospy.Subscriber("fiber_index", IntList, callback, (ax,fig_agg, window))
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+    window.close()
+
+
+if __name__ == '__main__':
+    listener()
+'''
+
+
+
+'''
+# make a thread for GUI and keep listerner as it is
+
+datareceived = []
+cv_image = []
+exitflag = 1
+countgloabal = 1
 
 def draw_figure(canvas_plot, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas_plot)
@@ -44,6 +339,7 @@ def draw_figure(canvas_plot, figure):
     return figure_canvas_agg
 
 def gui():
+    global countgloabal
     sg.theme('LightGreen')
     figure_w = 650
     figure_h = 650
@@ -164,6 +460,8 @@ def gui():
         fig_agg.draw()
         count = count + 1
 
+        print("count in GUI is:", countgloabal)
+
     # show camera image
         # imgbytes = cv.imencode('.png', cv_image)[1].tobytes()  # ditto
 
@@ -193,9 +491,12 @@ def callback(data):
     # cv.waitKey(3)
     global exitflag
     global datareceived
+    global countgloabal
 
     datareceived = data.data
-    print("I received:", datareceived)
+    # print("I received:", datareceived)
+    print("count = ", countgloabal)
+    countgloabal = countgloabal+1
 
     if exitflag==0:
       rospy.signal_shutdown("Program aborted!")
@@ -227,9 +528,15 @@ if __name__ == '__main__':
     # listener()
     a = threading.Thread(target = gui)
         # b = threading.Thread(target = printB)
+    # a.setDaemon(True)
+    
+    # b = threading.Thread(target = listener)
+        # b = threading.Thread(target = printB)
     a.setDaemon(True)
     a.start()
     listener()
+
+'''
 
 
 '''
